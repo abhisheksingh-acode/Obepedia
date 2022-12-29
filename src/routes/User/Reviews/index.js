4d3a63e11ca70c0e4c8509fd91708ebfbd33a561
wdAres <puneetsri9990@gmail.com>
2022-12-30 02:53:51 +0530

    Update index.js

--- src/routes/User/Reviews/index.js
+++ src/routes/User/Reviews/index.js
@@ -8,12 +8,13 @@ const reviewsmodel = require("../../../models/Reviews/reviews");
 // Posting Reviews
 const postReview = (req, resp) => {
   User.findOne({ _id: req.params.id }).then((val) => {
+
     const { name, desc, rating, ref_id ,course_id,
       institute_id } = req.body;
 
     const review = new reviewsmodel({
       _id: new mongoose.Types.ObjectId(),
-      name: val.name,
+      name: val.username,
       desc,
       rating,
       ref_id: req.params.id, // Referencing student
