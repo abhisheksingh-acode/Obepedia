29444a7d2a32d5e5866aaf3e4b9b1b9f9deea9e4
abhishek <abhishek8587singh@gmail.com>
2023-01-07 12:17:39 +0530

    fix query search

--- src/routes/User/Search/index.js
+++ src/routes/User/Search/index.js
@@ -11,25 +11,23 @@ const Search = async (req, resp) => {
   try {
     let response = await InstituteProfileModel.find({
       $or: [
-        { name: { $regex: req.params.key } },
-        { location: { $regex: req.params.key } },
+        { name: { $regex: `^${req.params.key}`, $options: "i"  } },
+        { location: { $regex: `${req.params.key}`, $options: "i"  } },
       ],
     });
     let response2 = await VacancyModel.find({
       $or: [
-        { name: { $regex: req.params.key } },
-        { responsibilities: { $regex: req.params.key } },
-        { skills: { $regex: req.params.key } },
-        { company: { $regex: req.params.key } },
-        { location: { $regex: req.params.key } },
-        { timming: { $regex: req.params.key } },
+        { name: { $regex: `^${req.params.key}`, $options: "i" } },
+        { responsibilities: { $regex: `^${req.params.key}`, $options: "i" } },
+        { skills: { $regex: `^${req.params.key}`, $options: "i" } },
+        { company: { $regex: `^${req.params.key}`, $options: "i" } },
+        { location: { $regex: `${req.params.key}`, $options: "i" } },
       ],
     });
     let response3 = await course.find({
       $or: [
-        { course: { $regex: req.params.key } },
+        { course: { $regex: `^${req.params.key}`, $options: "i" } },
         { medium: { $regex: req.params.key } },
-        { duration: { $regex: req.params.key } },
       ],
     });
 
@@ -47,10 +45,13 @@ const Search = async (req, resp) => {
 const SearchInstitute = async (req, resp) => {
   try {
     const { key, location } = req.query;
+
     let response = await InstituteProfileModel.find({
       $or: [
-        { name: new RegExp(key + "i") },
-        { location: new RegExp(location + "i") },
+        {
+          name: { $regex: `^${key}`, $options: "i" },
+        },
+        { location: { $regex: `${location}`, $options: "i" } },
       ],
     });
     let result = { response };
@@ -58,7 +59,7 @@ const SearchInstitute = async (req, resp) => {
   } catch (error) {
     let response = await InstituteProfileModel.find();
     let result2 = { response };
-    resp.status(200).json({ result: result2 });
+    resp.status(200).send(error);
   }
 };
 
