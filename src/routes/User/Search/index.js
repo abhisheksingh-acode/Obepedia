e979253082d9cf20e121dea0eb05683c70660140
abhishek <abhishek8587singh@gmail.com>
2023-01-07 11:40:41 +0530

    fix

--- src/routes/User/Search/index.js
+++ src/routes/User/Search/index.js
@@ -5,58 +5,58 @@ const course = require("../../../models/Course/course");
 
 // now import all required models
 const InstituteProfileModel = require("../../../models/Instituteprofile/instituteprofile");
-const VacancyModel = require('../../../models/Vacancy/vacancy')
-
+const VacancyModel = require("../../../models/Vacancy/vacancy");
 
 const Search = async (req, resp) => {
   try {
-    let response = await InstituteProfileModel.find(
-        {"$or": [
-            {'name' : {$regex : req.params.key}},
-            {'location' : {$regex : req.params.key}},
-        ]}
-    )
-    let response2 = await  VacancyModel.find(
-      {"$or": [
-        {'name' : {$regex : req.params.key}},
-        {'responsibilities' : {$regex : req.params.key}},
-        {'skills' : {$regex : req.params.key}},
-        {'company' : {$regex : req.params.key}},
-        {'location' : {$regex : req.params.key}},
-        {'timming' : {$regex : req.params.key}},
-    ]}
-    )
-    let response3 = await  course.find(
-      {"$or": [
-        {'course' : {$regex : req.params.key}},
-        {'medium' : {$regex : req.params.key}},
-        {'duration' : {$regex : req.params.key}}
-    ]}
-    )
+    let response = await InstituteProfileModel.find({
+      $or: [
+        { name: { $regex: req.params.key } },
+        { location: { $regex: req.params.key } },
+      ],
+    });
+    let response2 = await VacancyModel.find({
+      $or: [
+        { name: { $regex: req.params.key } },
+        { responsibilities: { $regex: req.params.key } },
+        { skills: { $regex: req.params.key } },
+        { company: { $regex: req.params.key } },
+        { location: { $regex: req.params.key } },
+        { timming: { $regex: req.params.key } },
+      ],
+    });
+    let response3 = await course.find({
+      $or: [
+        { course: { $regex: req.params.key } },
+        { medium: { $regex: req.params.key } },
+        { duration: { $regex: req.params.key } },
+      ],
+    });
 
-    let result = { institute:response , vacancy : response2 ,  course_list : response3 }
-    resp.status(200).json(result)
+    let result = {
+      institute: response,
+      vacancy: response2,
+      course_list: response3,
+    };
+    resp.status(200).json(result);
   } catch (error) {
-    resp.status(500).json(error)
+    resp.status(500).json(error);
   }
 };
- 
 
-const SearchInstitute = async (req,resp)=>{
+const SearchInstitute = async (req, resp) => {
   try {
-      const {name, location} = req.query;
-
-    let response = await InstituteProfileModel.find(
-      {"$or": [
-          {'name' : {$regex : name}},
-          {'location' : {$regex : location}}
-      ]}
-  );
-  let result = {response}
-  resp.status(200).json(result)
+    const { name, location } = req.query;
+    let response = await InstituteProfileModel.find({
+      $or: [{ name: { $regex: name } }, { location: { $regex: location } }],
+    });
+    let result = { response };
+    resp.status(200).json(result);
   } catch (error) {
-    resp.status(500).json(error)
+    let response2 = await InstituteProfileModel.find();
+    let result2 = { response2 };
+    resp.status(200).json({ result: result2 });
   }
-}
+};
 
-module.exports = { Search ,SearchInstitute };
+module.exports = { Search, SearchInstitute };
