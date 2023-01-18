df612c39f56878aede9afb39f46ee6cc0a45994b
abhishek <abhishek8587singh@gmail.com>
2023-01-18 18:06:11 +0530

    push

--- src/models/Sidebar/sidebar.js
+++ src/models/Sidebar/sidebar.js
@@ -1,16 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const SidebarSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    link:{
-        type:String,
-        required:true
-    },
-    banner:{
-        type:String,
-        required:true
-    }
-})
+  link: {
+    type: String,
+    required: true,
+  },
+  banner: {
+    type: String,
+    required: true,
+  },
+});
 
-
-module.exports = mongoose.model('sidebarmodel', SidebarSchema);
+module.exports = mongoose.model("sidebarmodel", SidebarSchema);
--- src/routes/Admin/Sidebar/index.js
+++ src/routes/Admin/Sidebar/index.js
@@ -1,24 +1,15 @@
 const express = require("express");
 const SidebarModel = require("../../../models/Sidebar/sidebar");
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const postSidebar = (req, resp) => {
+const postSidebar = async (req, resp) => {
   const { link, banner } = req.body;
-  const Sidebar = new SidebarModel({
-    _id: new mongoose.Types.ObjectId(),
-    link,
-    banner,
-  })
-    .save()
-    .then((result) => {
-      resp.status(200).json(result);
-    })
-    .catch((err) => {
-      resp.status(500).json({ err });
-    });
+  const Sidebar = await SidebarModel.insertMany(req.body);
+
+  resp.status(200).json({result : Sidebar});
 };
 
-const getSidebar =  (req, resp) => {
+const getSidebar = (req, resp) => {
   SidebarModel.find()
     .then((result) => {
       resp.status(200).json(result);
@@ -28,4 +19,4 @@ const getSidebar =  (req, resp) => {
     });
 };
 
-module.exports = { getSidebar , postSidebar};
+module.exports = { getSidebar, postSidebar };
