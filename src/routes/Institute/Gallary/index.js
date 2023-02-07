1cfbc433e55ec7eb873a0b25443f8a7339e62a9d
abhishek <abhishek8587singh@gmail.com>
2023-02-07 13:15:14 +0530

    multiple gallery delete

--- src/routes/Institute/Gallary/index.js
+++ src/routes/Institute/Gallary/index.js
@@ -4,11 +4,11 @@ const GallaryModel = require("../../../models/Gallary/gallary");
 
 // Adding Gallary Information
 const postGallary = (req, resp) => {
-  const { files , institute_id} = req.body;
+  const { files, institute_id } = req.body;
   const Gallary = new GallaryModel({
     _id: new mongoose.Types.ObjectId(),
     files,
-    institute_id : req.params.id
+    institute_id: req.params.id,
   })
     .save()
     .then((result) => {
@@ -20,10 +20,9 @@ const postGallary = (req, resp) => {
 };
 
 // Getting Gallary Information
-const getGallary = (req, resp) => 
-{
-  const institute_id = req.params.id
-  GallaryModel.find({institute_id})
+const getGallary = (req, resp) => {
+  const institute_id = req.params.id;
+  GallaryModel.find({ institute_id })
     .then((result) => {
       resp.status(200).json(result);
     })
@@ -33,16 +32,12 @@ const getGallary = (req, resp) =>
 };
 
 // Deleting Gallary
-const delGallary = (req,resp)=>{
-  GallaryModel.findOne({_id:req.params.id})
-  .deleteOne()
-  .then(result=>{
-    resp.status(200).json(result)
-  })
-  .catch(err=>{
-    resp.status(500).json({error:err})
-  })
-}
+const delGallary = async (req, resp) => {
+  req.body.ids.forEach(async (el, index) => {
+    await GallaryModel.findByIdAndDelete(el);
+  });
 
+  return resp.status(200).json({msg: "Delete operation performed successfuly."});
+};
 
-module.exports = {postGallary , getGallary , delGallary}
+module.exports = { postGallary, getGallary, delGallary };
--- src/routes/Institute/index.js
+++ src/routes/Institute/index.js
@@ -1,75 +1,94 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const TokenAuth = require('../../middlewares/TokenAuth')
+const TokenAuth = require("../../middlewares/TokenAuth");
 
-// Login Module 
-const {Login} = require('./Login/auth')
+// Login Module
+const { Login } = require("./Login/auth");
 
-router.post('/login' , Login)
+router.post("/login", Login);
 
-// Institute Details 
-const {InstiDetails , getInstiUsers} = require('./InstiDetails/index');
+// Institute Details
+const { InstiDetails, getInstiUsers } = require("./InstiDetails/index");
 
-router.post('/instidetails/:id' , TokenAuth , InstiDetails);
-router.get('/getinstiusers/:id' , TokenAuth , getInstiUsers);
+router.post("/instidetails/:id", TokenAuth, InstiDetails);
+router.get("/getinstiusers/:id", TokenAuth, getInstiUsers);
 
 // Profile Module
-const {postProfile, getProfile , delProfile , putProfile} = require('./Profile/index')
+const {
+  postProfile,
+  getProfile,
+  delProfile,
+  putProfile,
+} = require("./Profile/index");
 
-router.post('/profile/postprofile/:id' , TokenAuth ,  postProfile)
-router.get('/profile/getprofile/:id' , TokenAuth ,  getProfile)
-router.delete('/profile/delprofile/:id' , TokenAuth ,  delProfile)
-router.put('/profile/putprofile/:id' , TokenAuth ,  putProfile)
-
+router.post("/profile/postprofile/:id", TokenAuth, postProfile);
+router.get("/profile/getprofile/:id", TokenAuth, getProfile);
+router.delete("/profile/delprofile/:id", TokenAuth, delProfile);
+router.put("/profile/putprofile/:id", TokenAuth, putProfile);
 
 // faculty Module
-const {postfaculty, getfaculty , delfaculty} = require('./Faculty/index')
+const { postfaculty, getfaculty, delfaculty } = require("./Faculty/index");
 
-router.post('/faculty/postfaculty/:id' , TokenAuth ,  postfaculty)
-router.get('/faculty/getfaculty/:id' , TokenAuth ,  getfaculty)
-router.delete('/faculty/delfaculty/:id' , TokenAuth ,  delfaculty)
-
+router.post("/faculty/postfaculty/:id", TokenAuth, postfaculty);
+router.get("/faculty/getfaculty/:id", TokenAuth, getfaculty);
+router.delete("/faculty/delfaculty/:id", TokenAuth, delfaculty);
 
 // Vacancy Module
-const {postVacancy, getVacancy ,delVacancy , getVacancyByInstitute } = require('./Vacancy/index')
+const {
+  postVacancy,
+  getVacancy,
+  delVacancy,
+  getVacancyByInstitute,
+} = require("./Vacancy/index");
 
-router.post('/vacancy/postvacancy/:id' , postVacancy)
-router.get('/vacancy/getvacancy/:id' , getVacancy)
-router.get('/vacancy/getvacancybyinstitute/:id' , getVacancyByInstitute)
-router.delete('/vacancy/delvacancy/:id' , delVacancy)
-
-
+router.post("/vacancy/postvacancy/:id", postVacancy);
+router.get("/vacancy/getvacancy/:id", getVacancy);
+router.get("/vacancy/getvacancybyinstitute/:id", getVacancyByInstitute);
+router.delete("/vacancy/delvacancy/:id", delVacancy);
 
 // Course Module
-const {postCourse, getCourse ,delCourse} = require('./Course/index')
+const { postCourse, getCourse, delCourse } = require("./Course/index");
 
-router.post('/course/postcourse/:id' , postCourse)
-router.get('/course/getcourse/:id' , getCourse)
-router.delete('/course/delcourse/:id' , delCourse)
-
+router.post("/course/postcourse/:id", postCourse);
+router.get("/course/getcourse/:id", getCourse);
+router.delete("/course/delcourse/:id", delCourse);
 
 // Gallary Module
-const {postGallary, getGallary ,delGallary} = require('./Gallary/index')
+const { postGallary, getGallary, delGallary } = require("./Gallary/index");
 
-router.post('/gallary/postgallary/:id' , postGallary)
-router.get('/gallary/getgallary/:id' , getGallary)
-router.delete('/gallary/delgallary/:id' , delGallary)
-
+router.post("/gallary/postgallary/:id", postGallary);
+router.get("/gallary/getgallary/:id", getGallary);
+router.delete("/gallary/delgallary", delGallary);
 
 // Review Module
-const { getAllReviewsOnCourse , getAllReviewsOnInstitute, deleteReview, ReviewOnInstitute , ReviewOnCourse , ReviewByStudent ,postReviewOnCourse , postReviewOnInstitute }  = require('../Admin/Reviews/index')
+const {
+  getAllReviewsOnCourse,
+  getAllReviewsOnInstitute,
+  deleteReview,
+  ReviewOnInstitute,
+  ReviewOnCourse,
+  ReviewByStudent,
+  postReviewOnCourse,
+  postReviewOnInstitute,
+} = require("../Admin/Reviews/index");
 
+router.post("/reviews/postreviewoncourse/:id", TokenAuth, postReviewOnCourse);
+router.post(
+  "/reviews/postreviewoninstitute/:id",
+  TokenAuth,
+  postReviewOnInstitute
+);
 
-router.post('/reviews/postreviewoncourse/:id' , TokenAuth , postReviewOnCourse)
-router.post('/reviews/postreviewoninstitute/:id' , TokenAuth , postReviewOnInstitute)
+router.get("/reviews/reviewsoninstitute/:id", TokenAuth, ReviewOnInstitute);
+router.get("/reviews/reviewsoncourse/:id", TokenAuth, ReviewOnCourse);
+router.get(
+  "/reviews/allreviewsoninstitute",
+  TokenAuth,
+  getAllReviewsOnInstitute
+);
+router.get("/reviews/allreviewsoncourse", TokenAuth, getAllReviewsOnCourse);
 
-router.get('/reviews/reviewsoninstitute/:id' , TokenAuth , ReviewOnInstitute)
-router.get('/reviews/reviewsoncourse/:id' , TokenAuth , ReviewOnCourse)
-router.get('/reviews/allreviewsoninstitute' , TokenAuth , getAllReviewsOnInstitute)
-router.get('/reviews/allreviewsoncourse' , TokenAuth , getAllReviewsOnCourse)
+router.get("/reviews/reviewsbystudent/:id", TokenAuth, ReviewByStudent);
+router.delete("/reviews/deletereview/:id", TokenAuth, deleteReview);
 
-router.get('/reviews/reviewsbystudent/:id' , TokenAuth , ReviewByStudent)
-router.delete('/reviews/deletereview/:id' , TokenAuth , deleteReview)
-
-
-module.exports  = router ;
+module.exports = router;
